3121448ab2e013a4b42faa62595dc793511ae904
Zoë <>
2021-03-24 10:21:58 +0100

    Small format change

--- Arduino/arduino_grove___dust_sensor__ppd42ns__project_code.c
+++ Arduino/arduino_grove___dust_sensor__ppd42ns__project_code.c
@@ -1,15 +1,10 @@
-//Source Dust code https://create.arduino.cc/projecthub/infoelectorials/project-010-arduino-grove-dust-sensor-ppd42ns-project-ab5f5e
-
 int pin = 8;
 unsigned long duration;
 unsigned long starttime;
-unsigned long sampletime_ms = 30000; //Orignal sample time: 2000
-
+unsigned long sampletime_ms = 2000; 
 unsigned long lowpulseoccupancy = 0;
 float ratio = 0;
-float concentration = 0; 
-
-float m3 = 0.0283168466; //To calculate cubicmetre
+float concentration = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -23,13 +18,12 @@ void loop() {
   if ((millis()-starttime) >= sampletime_ms) //if the sampel time = = 30s
   {
     ratio = lowpulseoccupancy/(sampletime_ms*10.0);  
-    concentration = (1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62)* m3; 
-//    Serial.print("Concentration = ");
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; 
+    Serial.print("Concentration = ");
     Serial.print(concentration);
-    Serial.println(" \t ug/m^3"); //micro gram per kubic metre
+    Serial.println(" pcs/0.01cf");
     Serial.println("\n");
     lowpulseoccupancy = 0;
     starttime = millis();
-
   }
 }
